0ef6b19d725b6a75ab8b21f664dfef098ae1226f
Jim Bair <jbair@redhat.com>
2022-04-25 11:14:36 -0500

    Update koji_url

--- nvr-to-package.py
+++ nvr-to-package.py
@@ -5,6 +5,6 @@ if len(sys.argv) != 2:
     print("ERROR: please provide an nvr.")
     sys.exit(1)
 nvr = sys.argv[1]
-koji_url = "https://kojihub.stream.rdu2.redhat.com/kojihub"
+koji_url = "https://kojihub.stream.centos.org/kojihub"
 session = koji.ClientSession(koji_url)
 print(session.getBuild(nvr)['package_name'])
