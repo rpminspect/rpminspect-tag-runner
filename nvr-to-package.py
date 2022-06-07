2df5aa9a2e3b9ab3bf3370ae7fca1b36f35f962c
Jim Bair <jbair@redhat.com>
2022-06-07 10:54:08 -0500

    Bugfixes to the cache functionality

--- nvr-to-package.py
+++ nvr-to-package.py
@@ -11,10 +11,13 @@ nvr = sys.argv[1]
 
 # Load or initialize our cache as needed
 cache_file = 'nvr-to-package.json'
-f = open(cache_file,'w+')
-if f.tell() != 0:
-    data = json.load(f)
-else:
+# Exceptions could be missing file or invalid file
+# The cache is not vital so we can start from scratch
+# as needed
+try:
+    with open(cache_file, 'r') as f:
+        data = json.loads(f.read())
+except:
     data = {}
 
 # Check if we have it already
@@ -22,13 +25,19 @@ if nvr in data:
     print(data[nvr])
     sys.exit(0)
 
-# Fetch from koji, cache it, and return it
+# Fetch from koji and abort if missing
 koji_url = "https://kojihub.stream.centos.org/kojihub"
 session = koji.ClientSession(koji_url)
-result = session.getBuild(nvr)['package_name']
+try:
+    result = session.getBuild(nvr)['package_name']
+except:
+    print(f"Failed to find package name for {nvr}")
+    sys.exit(1)
+
+# Cache it and return it
 data[nvr] = result
-f.write(str(data))
-f.close()
+with open(cache_file, 'w') as f:
+    f.write(json.dumps(data))
 print(result)
 
 # all done
