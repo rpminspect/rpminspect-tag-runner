9a82d55f5037b1e94ddd518f64f9dbdb097e3d12
Jim Bair <james.d.bair@gmail.com>
2022-06-26 14:15:47 -0500

    Proper validation for missing or failed KOJI_CMD call

--- generate-list.sh
+++ generate-list.sh
@@ -6,13 +6,23 @@ KOJI_CMD='koji -p stream'
 KOJI_TAG='c9s-pending'
 LIST='list.txt'
 
+# Required to properly catch KOJI_CMD failures
+set -o pipefail
+
 # Hide the ugly error messages from the user
 # DeprecationWarning: The stub function for translation is no longer used
 echo "$(date) - INFO: Generating ${LIST}"
 ${KOJI_CMD} list-tagged --inherit --latest ${KOJI_TAG} 2>/dev/null | tail -n +3 | awk '{print $1}' > ${LIST}
 
+# Check if KOJI_CMD is installed
+ec="$?"
+if [[ ${ec} -eq 127 ]]; then
+  echo "ERROR: The requested program '${KOJI_CMD}' is not installed."
+  exit 1
+fi
+
 # See if we had a clean error and a non-empty file
-if [[ $? -ne 0 ]]; then
+if [[ ${ec} -ne 0 ]]; then
   echo "ERROR: list generation koji call failed" >&2
   exit 1
 elif [[ ! -s ${LIST} ]]; then
